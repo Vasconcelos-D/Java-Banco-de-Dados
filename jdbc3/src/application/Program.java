1b8f66363da92598eb0df2b6d1481b7ae5b1d69e
Vasconcelos-D <roronoazoroaf@gmail.com>
2024-09-12 09:36:40 -0300

    Inserindo novos dados recuperando o ID

--- jdbc3/src/application/Program.java
+++ jdbc3/src/application/Program.java
@@ -2,7 +2,9 @@ package application;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
@@ -20,13 +22,15 @@ public class Program {
 
 	            // Estabelece a conexão com o banco de dados
 	            conn = DB.getConnection();
-
+/*
 	            // Prepara a declaração SQL com placeholders (?) para os valores
 	            st = conn.prepareStatement(
 	                "INSERT INTO seller "
 	                + "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
 	                + "VALUES"
-	                + "(?, ?, ?, ?, ?)"  // Define os 5 placeholders para os valores a serem inseridos
+	                + "(?, ?, ?, ?, ?)",
+	                Statement.RETURN_GENERATED_KEYS
+	                // Define os 5 placeholders para os valores a serem inseridos
 	            );
 
 	            // Define o valor para o primeiro placeholder (nome do vendedor)
@@ -43,21 +47,30 @@ public class Program {
 
 	            // Define o valor para o quinto placeholder (ID do departamento)
 	            st.setInt(5, 4);
-
+*/
+	            
+	            // Prepara uma nova declaração SQL para inserir valores na tabela department
+	            st = conn.prepareStatement("insert into department (Name) values ('D3'), ('D4')", Statement.RETURN_GENERATED_KEYS);
+	            
 	            // Executa a instrução SQL e obtém o número de linhas afetadas
 	            int rowsAffected = st.executeUpdate();
 
-	            // Exibe no console quantas linhas foram afetadas
-	            System.out.println("Done! Rows affected: " + rowsAffected);
+	            if (rowsAffected > 0) {
+	            	ResultSet rs = st.getGeneratedKeys();
+	            	while (rs.next()) {
+	            		int id = rs.getInt(1);
+	            		System.out.println("Done Id = " + id);
+	            	}
+	            }
+	            else {
+	            	System.out.println("No rown affected! ");
+	            }
 	        }
 	        catch (SQLException e) {
 	            // Trata possíveis erros de SQL
 	            e.printStackTrace();
 	        }
-	        catch (ParseException e) {
-	            // Trata erros de conversão de data
-	            e.printStackTrace();
-	        }
+
 	        finally {
 	            // Fecha a declaração e a conexão
 	            DB.closeStatement(st);
